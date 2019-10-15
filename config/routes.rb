b9f89197ce661895ce4423d7440d5b5f8e01bfeb
Seba_virtual <rata_6952@hotmail.com>
2019-10-15 16:13:04 -0300

    update index

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   devise_for :users
   #patch 'todos/:todo_id', to: 'todos#update', as: 'todos_completed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root 'tasks#index'
 end
